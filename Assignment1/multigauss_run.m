332d04569c98e7c5b71f76a4e9bbab8fdc60de81
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-02-06 22:30:51 +0100

    Mere update.

--- Assignment1/multigauss_run.m
+++ Assignment1/multigauss_run.m
@@ -6,13 +6,23 @@ points=zeros(2,N);
 for j=1:N,
     z = randn(2,1);
     points(:, j) = multigauss( mu, L, z );
-    %points(:, multigauss( mu, L, z ))
 end
 
 X = points(1,:);
 Y = points(2,:);
 meanX = mean(X);
 meanY = mean(Y);
+
+sigmaML = zeros(2,2);
+for i=1:N,
+    sigmaML = sigmaML + (points(:,i) - mu) * (points(:,i) - mu)';
+end
+sigmaML = sigmaML ./ N;
+[eigenVectors,eigenValues] = eig(sigmaML);
+
+e1 = mu + sqrt(eigenValues(1))*eigenVectors(:,1)
+e2 = mu + sqrt(eigenValues(2))*eigenVectors(:,2)
+
 plot(X,Y,'x',meanX,meanY,'o',mu(1),mu(2),'o');
 title('Multivariate Gaussian Distribution for N=100','FontSize',15);
 grid on;
