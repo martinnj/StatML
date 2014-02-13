e91eb5c1905257cbc721613735641fb9de65a76a
Martin Jørgensen <martinnjorgensen@gmail.com>
2014-02-13 21:14:40 +0100

    Seeded random generator, and proper mu in I.2.4.

--- Assignment1/multigauss_run.m
+++ Assignment1/multigauss_run.m
@@ -1,8 +1,11 @@
+clear all;
+rng(42); % Seed random generator to hvae consistent results.
+
 %%%% I.2.2
 mu = [1,2]';
 Sigma = [0.3,0.2;0.2,0.2];
 L = chol(Sigma,'lower');
-N=1000;
+N=100;
 points=zeros(2,N);
 for j=1:N,
     z = randn(2,1);
@@ -13,24 +16,25 @@ X = points(1,:);
 Y = points(2,:);
 
 plot(X, Y, 'x');
-title('Multivariate Gaussian distribution for N=1000', 'FontSize', 15);
+title('Multivariate Gaussian distribution for N=100', 'FontSize', 15);
 
 %%%% I.2.3
 meanX = mean(X);
 meanY = mean(Y);
+muML = [meanX; meanY];
 plot(X, Y, 'x', meanX, meanY, 'o', mu(1), mu(2), 'o');
 axis equal;
 grid on;
-title('Multivariate Gaussian distribution for N=1000 with sample mean and distribution mean', 'FontSize', 15);
+title('Multivariate Gaussian distribution for N=100 with sample mean and distribution mean', 'FontSize', 15);
 legend('Multivariate Gaussian Distribution', 'Sample mean', 'Distribution Mean');
 
 %%%%% I.2.4
 %%%%% SigmaML = Maximum likelihood
 SigmaML = zeros(2,2);
 for i=1:N,
-    SigmaML = SigmaML + (points(:,i) - mu) * (points(:,i) - mu)';
+    SigmaML = SigmaML + (points(:,i) - muML) * (points(:,i) - muML)';
 end
-SigmaML = SigmaML ./ N;
+SigmaML = SigmaML ./ N
 
 [eigenVectors,eigenValues] = eig(SigmaML);
 
@@ -81,7 +85,7 @@ for j=1:N,
 end
 X = points(1,:);
 Y = points(2,:);
-plot(X, Y, 'x','Color','cyan');
+plot(X, Y, 'x','Color','blue');
 
 L = chol(Sigma90,'lower');
 points=zeros(2,N);
@@ -110,6 +114,6 @@ Y = points(2,:);
 plot(X, Y, 'x','Color','black');
 axis equal;
 grid on;
-title('Rotated Multivariate Gaussian distribution for N=1000', 'FontSize', 15);
+title('Rotated Multivariate Gaussian distribution for N=100', 'FontSize', 15);
 legend('Sigma rotated 30 degrees','Sigma rotated 60 degrees','Sigma rotated 90 degrees','Sigma rotated to match X-axis')
 hold off;
