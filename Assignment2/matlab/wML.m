d032ac99d1a8b040425402bf56f41ce6ede1fe3d
Martin Grunbaum <martin@itsolveonline.net>
2014-02-27 15:20:03 +0000

    Skub til repo'et

--- Assignment2/matlab/wML.m
+++ Assignment2/matlab/wML.m
@@ -1,12 +1,4 @@
-function [ out ] = wML( Phi, t, D )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-tD = [];
-for i=1:D
-    tD = vertcat(tD,t);
-end
-
-out = pinv(Phi'*Phi) * (Phi'*tD);
-
+function [ out ] = wML( Phi, t)
+    out = pinv(Phi'*Phi) * (Phi'*t);
 end
 
--- Assignment2/matlab/woooooork.m
+++ Assignment2/matlab/woooooork.m
@@ -4,12 +4,10 @@ train = dlmread('sunspotsTrainStatML.dt');
 
 l = length(train);
 
-Phi1 = horzcat(ones(l,1),train(:,3),train(:,3));
-Phi1 = vertcat(Phi1, (horzcat(ones(l,1),train(:,4),train(:,4))));
+Phi1 = horzcat(ones(l,1),train(:,3),train(:,4));
 Phi2 = horzcat(ones(l,1),train(:,5));
-Phi3 = vertcat(train(:,1),train(:,2),train(:,3),train(:,4),train(:,5));
-Phi3 = horzcat(ones(5*l,1),Phi3,Phi3,Phi3,Phi3,Phi3);
+Phi3 = horzcat(ones(l,1),train(:,1),train(:,2),train(:,3),train(:,4),train(:,5));
 
-wML1 = wML(Phi1, train(:,6), 2)
-wML2 = wML(Phi2, train(:,6), 1)
-wML3 = wML(Phi3, train(:,6), 5)
+wML1 = wML(Phi1, train(:,6))
+wML2 = wML(Phi2, train(:,6))
+wML3 = wML(Phi3, train(:,6))
