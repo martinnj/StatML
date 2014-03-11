8148e2cc507e056fe74bee9d64dffe07f17cf122
Martin Grunbaum <martin@itsolveonline.net>
2014-03-11 16:11:59 +0000

    More NN shite.

--- Assignment3/matlab/III_1_1.m
+++ Assignment3/matlab/III_1_1.m
@@ -4,14 +4,35 @@ rng(42);
 train       = dlmread('sincTrain25.dt');
 validate    = dlmread('sincValidate10.dt');
 
-X = vertcat(ones(1,length(train)), train(:,1)')
-y = train(:,2)
+X = vertcat(ones(1,length(train)), train(:,1)');
+y = train(:,2);
 
 K = 1;
 D = 1;
 M = 2;
 
+h = @(a) a/(1+abs(a));
+dh = @(a) 1/(1+abs(a))^2;
+id = @(x) x;
+
 [wMD,wKM] = initialWeightsNN(K, M, D);
 
-firstTry = forwardProp(X, wMD, wKM, @(x) x);
-error = firstTry - y
+% Step 1: Forward Propagation.
+output = forwardProp(X, wMD, wKM, id, h);
+
+% Step 2: Calculate the output node errors.
+error = output - y
+
+% Step 3: Calculate hidden node errors.
+hidden_errors = zeros(1,M+1);
+for j=1:M+1
+    v = dh(output(j));
+    % OMG WORST CODE EVER> USE A SUM, F00
+    s = 0;
+    for k=1:K
+        s = s + (wKM(k,j) * output(k));
+    end
+    hidden_errors(j) = v*s;
+end
+hidden_errors
+% TODO: Do something with the hidden errors!
--- Assignment3/matlab/forwardProp.m
+++ Assignment3/matlab/forwardProp.m
@@ -1,4 +1,4 @@
-function [ out ] = forwardProp( X, wMD, wKM, y_act )
+function [ out ] = forwardProp( X, wMD, wKM, y_act, h )
 % Invariants: 
 %   K=Number of output neurons
 %   D=Dimensionality of input samples
@@ -11,8 +11,30 @@ function [ out ] = forwardProp( X, wMD, wKM, y_act )
 %   wKM is a  K x M matrix
 %  
 %   The first row of wMD should be [1; 0; 0; ...; 0]
-    h = @(a) a/(1+abs(a));
-    D = length(X);
-    wMDSize = size(wMD);
-    out = y_act(wKM * wMD * X)';
+
+    K = size(wKM,1);
+    M = size(wMD,1);
+    D = size(wMD,2);
+    out = zeros(K,length(X));
+    
+    % For every sample
+    for x=1:length(X)
+        sample = X(:,x);
+        % For every output node
+        for k=1:K
+            z = zeros(1,M);
+            % For every hidden node
+            for j=1:M    
+                % For every input node
+                for i=1:D
+                    w_ji = wMD(j,i);
+                    z(1,j) = z(1,j) + (w_ji * sample(i)); % Update value in hidden node j
+                end
+                z(1,j) = h(z(1,j)); % Apply activation function to the hidden node j.
+            end
+            % Output node k for sample x
+            out(k,x) = y_act(sum(z));
+        end
+    end
+    out = out';
 end
