2fd3744b45b92f0049195fc57172534c6078e00c
Martin Grunbaum <martin@itsolveonline.net>
2014-03-18 17:26:49 +0000

    III_1_2.m

--- Assignment3/matlab/III_1_1.m
+++ Assignment3/matlab/III_1_1.m
@@ -6,8 +6,8 @@ validate    = dlmread('sincValidate10.dt');
 
 X = vertcat(ones(1,length(train)), train(:,1)');
 y = train(:,2);
-%X_test = vertcat(ones(1,length(train)), validate(:,1)');
-%y_test = validate(:,2);
+X_validate = vertcat(ones(1,length(validate)), validate(:,1)');
+y_validate = validate(:,2);
 
 K = 1;
 D = 1;
@@ -19,15 +19,4 @@ learningRate = 0.01;
 stopDifference = 0.03;
 
 % Compute gradients using arbitrary sample data.
-[wMD, wKM, errors] = nnTrain(X, y, h, dh, K, M, D, learningRate, stopDifference);
-
-
-hold on;
-plot(X(2,:),y,'ob');
- 
-preds = zeros(1,length(X));
-for x=1:length(X)
-    [~,~,y_pred] = forwardProp(X(:,x), h, wMD, wKM);
-    preds(1,x) =  y_pred;
-end
-plot(X(2,:),preds(1,:),'or');
+[wMD, wKM, errors_train, errors_val] = nnTrain(X, y, X_validate, y_validate, h, dh, K, M, D, learningRate, stopDifference);
