f76025d30ef318fe9ad527a582aaf3d894a15484
ThomasNB <barnholdt@gmail.com>
2014-03-18 18:25:56 +0100

    2.1 og 2.2 kode rettet
    HURRA

--- Assignment3/matlab/III_2_2.m
+++ Assignment3/matlab/III_2_2.m
@@ -21,7 +21,8 @@ testY = test(:, testSize);
 % Get stuff before it's normalized.
 Cs = [0.01, 0.1, 1, 10, 100, 1000, 10000];
 gammas = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100];
-[preNormBestC, preNormBestgamma, preNormBestAcc] = fivefoldSVM(trainX, trainY, testX, testY, Cs, gammas)
+[preNormBestC, preNormBestgamma] = fivefoldSVM(trainX, trainY, testX, testY, Cs, gammas)
+
 
 % Normalize it!
 testX = scale(trainX,testX);
@@ -34,6 +35,26 @@ testVar = var(testX);
 trainY = train(:, trainSize);
 testY = test(:, testSize);
 
-
 % Lets test after data normalization.
-[normBestC, normBestgamma, normBestAcc] = fivefoldSVM(trainX, trainY, testX, testY, Cs, gammas)
+[normBestC, normBestgamma] = fivefoldSVM(trainX, trainY, testX, testY, Cs, gammas)
+
+
+%Using the best C and Gamma on preNorm
+preNormBestSigma = sqrt(1/(2*preNormBestgamma));
+preNormModel = svmtrain(trainX, trainY, 'kernel_function','rbf','rbf_sigma',preNormBestSigma,'boxconstraint',preNormBestC);
+
+preNormTrainPredY = svmclassify(preNormModel,trainX);
+preNormTestPredY = svmclassify(preNormModel,testX);
+
+preNormTrainAccuracy = 1 - (nnz(preNormTrainPredY - trainY)) / length(preNormTrainPredY)
+preNormTestAccuracy = 1 - (nnz(preNormTestPredY - testY)) / length(preNormTestPredY)
+
+%Using the best C and Gamma on norm
+normBestSigma = sqrt(1/(2*normBestgamma));
+normModel = svmtrain(trainX, trainY, 'kernel_function','rbf','rbf_sigma',normBestSigma,'boxconstraint',normBestC);
+
+normTrainPredY = svmclassify(normModel,trainX);
+normTestPredY = svmclassify(normModel,testX);
+
+normTrainAccuracy = 1 - (nnz(normTrainPredY - trainY)) / length(normTrainPredY)
+normTestAccuracy = 1 - (nnz(normTestPredY - testY)) / length(normTestPredY)
